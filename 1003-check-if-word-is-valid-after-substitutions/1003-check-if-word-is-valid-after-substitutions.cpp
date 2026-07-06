04870182afa9f06a0b4920cb09dc170114560ef9
Royalsaurabh8055 <102569388+Royalsaurabh8055@users.noreply.github.com>
2026-07-07 03:58:07 +0530

    Time: 539 ms (5.11%), Space: 683.2 MB (5.11%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size() ==0) return true;
+
+        int found = s.find("abc");
+
+        if(found != string::npos)
+        {
+            string tleft = s.substr(0,found);
+            string tright = s.substr(found+3,s.size());
+
+           return isValid(tleft+tright);
+        }
+        return false;
+    }
+};
