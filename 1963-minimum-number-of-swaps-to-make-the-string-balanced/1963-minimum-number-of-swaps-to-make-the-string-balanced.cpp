9e6637480f870c0b5b60e48bdaf8a5e6900b4830
Royalsaurabh8055 <102569388+Royalsaurabh8055@users.noreply.github.com>
2026-07-08 14:34:33 +0530

    Time: 15 ms (43.51%), Space: 34.2 MB (64.92%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -2,36 +2,58 @@ class Solution {
 public:
     int minSwaps(string s) {
         
-        stack<char>st; 
+//         stack<char>st; 
 
-        for(int i =0;i<s.length();i++)
+//         for(int i =0;i<s.length();i++)
+//         {
+//             char ch = s[i];
+
+//             if(ch == '[')
+//             st.push(ch); // if open found simply push we need to check for close ones
+
+//             else // closing brackets found
+//             {
+//                 if(!st.empty() && st.top() == '[') // top pr open hai
+//                 st.pop(); // cancel valid pairs
+
+//                 else
+//                 st.push(ch);
+//             }
+//         }
+//         int open =0;
+//         int close =0;
+//         while(!st.empty())
+//         {
+//             if(st.top() == '[') open++;
+
+//             else
+//             close++;
+
+//             st.pop();
+//         }
+
+//         return (open+1)/2;
+
+    int open =0;
+    int imbalance =0;
+
+    for(int i=0;i<s.length();i++)
+    {
+        char ch = s[i];
+
+        if(ch == '[') open++;
+
+        else// its ]
         {
-            char ch = s[i];
-
-            if(ch == '[')
-            st.push(ch); // if open found simply push we need to check for close ones
-
-            else // closing brackets found
-            {
-                if(!st.empty() && st.top() == '[') // top pr open hai
-                st.pop(); // cancel valid pairs
-
-                else
-                st.push(ch);
-            }
-        }
-        int open =0;
-        int close =0;
-        while(!st.empty())
-        {
-            if(st.top() == '[') open++;
+            if(open>0)
+            open--;
 
             else
-            close++;
-
-            st.pop();
+            imbalance++;
         }
+    }
 
-        return (open+1)/2;
+    return (imbalance+1)/2;
+
     }
 };
