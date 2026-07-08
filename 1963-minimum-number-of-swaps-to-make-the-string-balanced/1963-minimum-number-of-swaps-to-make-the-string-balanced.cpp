04de9b127b3267e85781cdc0ee3912803effdf96
Royalsaurabh8055 <102569388+Royalsaurabh8055@users.noreply.github.com>
2026-07-08 14:30:26 +0530

    Time: 30 ms (16.33%), Space: 37.6 MB (16.17%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        
+        stack<char>st; 
+
+        for(int i =0;i<s.length();i++)
+        {
+            char ch = s[i];
+
+            if(ch == '[')
+            st.push(ch); // if open found simply push we need to check for close ones
+
+            else // closing brackets found
+            {
+                if(!st.empty() && st.top() == '[') // top pr open hai
+                st.pop(); // cancel valid pairs
+
+                else
+                st.push(ch);
+            }
+        }
+        int open =0;
+        int close =0;
+        while(!st.empty())
+        {
+            if(st.top() == '[') open++;
+
+            else
+            close++;
+
+            st.pop();
+        }
+
+        return (open+1)/2;
+    }
+};
