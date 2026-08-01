00f44fd35eb3b0004491c28ced90c9a62e2c79dd
Saurabh Singh <102569388+Royalsaurabh8055@users.noreply.github.com>
2026-08-02 04:43:04 +0530

    Time: 71 ms (50.65%), Space: 82.5 MB (46.91%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    vector<vector<int>> ans;
+    vector<int> temp;
+
+    void solve(int start, int n, int k) {
+
+        // If we have selected k numbers
+        if (temp.size() == k) {
+            ans.push_back(temp);
+            return;
+        }
+
+        // Try every possible number
+        for (int i = start; i <= n; i++) {
+
+            temp.push_back(i);
+
+            solve(i + 1, n, k);
+
+            temp.pop_back();    // Backtrack
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+
+        solve(1, n, k);
+
+        return ans;
+    }
+};
