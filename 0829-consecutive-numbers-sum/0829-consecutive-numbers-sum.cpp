e6ee5e2bbb91968fa3a110176bda2abbebf4c608
Saurabh Singh <102569388+Royalsaurabh8055@users.noreply.github.com>
2026-09-08 21:04:46 +0530

    Time: 3 ms (38.59%), Space: 7.9 MB (37.3%) - LeetHub

---
+++ 0829-consecutive-numbers-sum/0829-consecutive-numbers-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int consecutiveNumbersSum(int n) {
+
+        int ans = 0;
+
+        for (int k = 1; k * (k - 1) / 2 < n; k++) {
+
+            int remaining = n - k * (k - 1) / 2;
+
+            if (remaining % k == 0) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
