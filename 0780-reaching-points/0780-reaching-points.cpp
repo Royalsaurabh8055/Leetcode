198f6c408e9bbb3b57f62689762cb0a823b83b4b
Saurabh Singh <102569388+Royalsaurabh8055@users.noreply.github.com>
2026-09-16 22:10:43 +0530

    Time: 0 ms (100%), Space: 7.9 MB (35.42%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+
+        while(tx > sx && ty > sy) {
+
+            if(tx > ty)
+                tx %= ty;
+            else
+                ty %= tx;
+        }
+
+        // Now one coordinate must be exactly equal
+        if(tx == sx && ty >= sy) {
+            return (ty - sy) % sx == 0;
+        }
+
+        if(ty == sy && tx >= sx) {
+            return (tx - sx) % sy == 0;
+        }
+
+        return false;
+    }
+};
