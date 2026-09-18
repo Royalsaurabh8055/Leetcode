d67be638e5a5105643a48622fc27fa2e95f25718
Saurabh Singh <102569388+Royalsaurabh8055@users.noreply.github.com>
2026-09-18 20:15:18 +0530

    Time: 0 ms (100%), Space: 22.7 MB (99.6%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+
+        // Sort by (minimum - actual) in descending order
+        sort(tasks.begin(), tasks.end(), [](const vector<int>& a,
+                                           const vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
+        });
+
+        int ans = 0;
+        int energy = 0;
+
+        for(auto &task : tasks) {
+
+            int actual = task[0];
+            int minimum = task[1];
+
+            // Need more energy to start this task
+            if(energy < minimum) {
+                ans += minimum - energy;
+                energy = minimum;
+            }
+
+            // Complete the task
+            energy -= actual;
+        }
+
+        return ans;
+    }
+};
